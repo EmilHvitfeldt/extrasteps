b156bc618357b20f40bb476debc75cbff1e46631
EmilHvitfeldt <emilhhvitfeldt@gmail.com>
2021-08-03 11:08:20 -0700

    add step_yeet

--- R/aaa.R
+++ R/aaa.R
@@ -1,4 +1,6 @@
-#' @importFrom recipes sel2char is_trained
+#' @importFrom recipes sel2char is_trained format_ch_vec format_selectors
+#' @importFrom recipes has_type
+#' @importFrom rlang na_chr
 #' @importFrom tibble tibble
 simple_terms <- function(x, ...) {
   if (is_trained(x)) {
