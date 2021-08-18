7506baa115c5ea9e7514d424130786413b480816
EmilHvitfeldt <emilhhvitfeldt@gmail.com>
2021-08-17 18:15:46 -0700

    add step_date_nearest

---
+++ R/date_nearest.R
@@ -0,0 +1,286 @@
+#' Time to Nearest Recurrent Date Time Event
+#'
+#' `step_date_nearest` creates a *specification* of a recipe
+#'  step that will create new columns indicating the time to nearest
+#'  recurrent event.
+#'
+#' @inheritParams recipes::step_center
+#' @param rules Named list of `almanac` rules.
+#' @param transform A function or character indication a function used oon the
+#'  resulting variables. See details for allowed names and their functions.
+#' @param columns A character string of variables that will be
+#'  used as inputs. This field is a placeholder and will be
+#'  populated once [prep.recipe()] is used.
+#' @return An updated version of `recipe` with the new check added to the
+#'  sequence of any existing operations.
+#' @export
+#'
+#' @details
+#'
+#' The `transform` argument can be function that takes a numeric vector and
+#' returns a numeric vector of the same length. It can also be a character
+#' vector, below is the supported vector names. Some functions come with offset
+#' to avoid `Inf`.
+#'
+#' ```r
+#' "identity"
+#' function(x) x
+#'
+#' "inverse"
+#' function(x) 1 / (x + 0.5)
+#'
+#' "exp"
+#' function(x) exp(x)
+#'
+#' "log"
+#' function(x) log(x + 0.5)
+#' ```
+#'
+#' The effect of `transform` is illustrated below.
+#'
+#'
+#' ```{r date_nearest, echo=FALSE, message=FALSE}
+#' library(almanac)
+#' library(ggplot2)
+#' library(dplyr)
+#' library(recipes)
+#'
+#' examples <- tibble(
+#' date1 = as.Date("2021-01-01") + 0:13
+#' )
+#'
+#' mondays <- weekly() %>% recur_on_wday("Monday")
+#'
+#' recipe(~., data = examples) %>%
+#'   step_date_nearest(date1,
+#'                    rules = list(monday = mondays),
+#'                    transform = "identity") %>%
+#'   prep() %>%
+#'   bake(new_data = NULL) %>%
+#'   bind_cols(examples) %>%
+#'   ggplot(aes(date1, date1_nearest_monday)) +
+#'   geom_col() +
+#'   theme_minimal() +
+#'   labs(title = "Days nearest Mondays in January 2021",
+#'        subtitle = "Without transformation",
+#'        y = NULL, x = NULL) +
+#'   scale_x_date(date_breaks = "1 day", date_labels = "%d")
+#'
+#' recipe(~., data = examples) %>%
+#'   step_date_nearest(date1,
+#'                    rules = list(monday = mondays),
+#'                    transform = "inverse") %>%
+#'   prep() %>%
+#'   bake(new_data = NULL) %>%
+#'   bind_cols(examples) %>%
+#'   ggplot(aes(date1, date1_nearest_monday)) +
+#'   geom_col() +
+#'   theme_minimal() +
+#'   labs(title = "Days nearest Mondays in January 2021",
+#'        subtitle = "With \"inverse\" transformation",
+#'        y = NULL, x = NULL) +
+#'   scale_x_date(date_breaks = "1 day", date_labels = "%d")
+#'
+#' recipe(~., data = examples) %>%
+#'   step_date_nearest(date1,
+#'                    rules = list(monday = mondays),
+#'                    transform = "exp") %>%
+#'   prep() %>%
+#'   bake(new_data = NULL) %>%
+#'   bind_cols(examples) %>%
+#'   ggplot(aes(date1, date1_nearest_monday)) +
+#'   geom_col() +
+#'   theme_minimal() +
+#'   labs(title = "Days nearest Mondays in January 2021",
+#'        subtitle = "With \"exp\" transformation",
+#'        y = NULL, x = NULL) +
+#'   scale_x_date(date_breaks = "1 day", date_labels = "%d")
+#'
+#' recipe(~., data = examples) %>%
+#'   step_date_nearest(date1,
+#'                    rules = list(monday = mondays),
+#'                    transform = "log") %>%
+#'   prep() %>%
+#'   bake(new_data = NULL) %>%
+#'   bind_cols(examples) %>%
+#'   ggplot(aes(date1, date1_nearest_monday)) +
+#'   geom_col() +
+#'   theme_minimal() +
+#'   labs(title = "Days nearest Mondays in January 2021",
+#'        subtitle = "With \"log\" transformation",
+#'        y = NULL, x = NULL) +
+#'   scale_x_date(date_breaks = "1 day", date_labels = "%d")
+#' ```
+#'
+#' The naming of the resulting variables will be on the form
+#'
+#' ```r
+#' {variable name}_nearest_{name of rule}
+#' ```
+#'
+#' @examples
+#' library(recipes)
+#' library(extrasteps)
+#' library(almanac)
+#' library(modeldata)
+#'
+#' data(Chicago)
+#'
+#' on_easter <- yearly() %>% recur_on_easter()
+#' on_weekend <- weekly() %>% recur_on_weekends()
+#'
+#' rules <- list(easter = on_easter, weekend = on_weekend)
+#'
+#' rec_spec <- recipe(ridership ~ date, data = Chicago) %>%
+#'   step_date_nearest(date, rules = rules)
+#'
+#' rec_spec_preped <- prep(rec_spec)
+#'
+#' bake(rec_spec_preped, new_data = NULL)
+step_date_nearest <-
+  function(recipe,
+           ...,
+           role = "predictor",
+           trained = FALSE,
+           rules = list(),
+           transform = "identity",
+           columns = NULL,
+           skip = FALSE,
+           id = rand_id("date_nearest")) {
+
+    add_step(
+      recipe,
+      step_date_nearest_new(
+        terms = ellipse_check(...),
+        trained = trained,
+        role = role,
+        rules = rules,
+        transform = transform,
+        columns = columns,
+        skip = skip,
+        id = id
+      )
+    )
+  }
+
+step_date_nearest_new <-
+  function(terms, role, trained, rules, transform, columns, skip, id) {
+    step(
+      subclass = "date_nearest",
+      terms = terms,
+      role = role,
+      trained = trained,
+      rules = rules,
+      transform = transform,
+      columns = columns,
+      skip = skip,
+      id = id
+    )
+  }
+
+#' @export
+prep.step_date_nearest <- function(x, training, info = NULL, ...) {
+  col_names <- recipes_eval_select(x$terms, training, info)
+
+  date_data <- info[info$variable %in% col_names, ]
+  if (any(date_data$type != "date"))
+    rlang::abort(
+      paste0("All variables for `step_date` should be either `Date` or",
+             "`POSIXct` classes."
+      )
+    )
+
+  if (is.null(names(x$rules)) || !is.list(x$rules)) {
+    rlang::abort(
+      "`rules` must be a named list."
+    )
+  }
+
+  if (!all(purrr::map_lgl(x$rules, inherits, "rschedule"))) {
+    rlang::abort(
+      "All `rules` must be `rschedule`s from {almanac}"
+    )
+  }
+
+  step_date_nearest_new(
+    terms = x$terms,
+    role = x$role,
+    trained = TRUE,
+    rules = x$rules,
+    transform = x$transform,
+    columns = col_names,
+    skip = x$skip,
+    id = x$id
+  )
+}
+
+#' @export
+bake.step_date_nearest <- function(object, new_data, ...) {
+
+  transform <- fetch_date_transforms(object$transform)
+
+  new_columns <- purrr::imap(object$columns, date_nearest_helper,
+                             new_data, object$rules, transform)
+
+  new_data <- dplyr::bind_cols(new_data, new_columns)
+  new_data <- dplyr::select(new_data, -names(object$columns))
+  new_data
+}
+
+date_transforms <- list(
+  identity = identity,
+  inverse = function(x) 1 / (x + 0.5),
+  exp = exp,
+  log = function(x) log(x + 0.5)
+)
+
+fetch_date_transforms <- function(x) {
+  if (is.function(x)) {
+    return(x)
+  }
+  if (!x %in% names(date_transforms)) {
+    rlang::abort(
+      paste("`transform` must be a function or one of built-in names.",
+            "See `?step_date_nearest` for valid input.")
+    )
+  }
+  date_transforms[[x]]
+}
+
+date_nearest_helper <- function(columnn, name, new_data, rule, transform) {
+  res <- purrr::map_dfc(rule, ~ {
+    values <- new_data[[columnn]]
+
+    before <- alma_next(values, .x, inclusive = TRUE) - values
+    after <- values - alma_previous(values, .x, inclusive = TRUE)
+    res <- pmin(before, after)
+    res <- as.numeric(res)
+    res <- transform(res)
+    res
+  }
+  )
+
+  names(res) <- paste(name, "nearest", names(res), sep = "_")
+  res
+}
+
+print.step_date_nearest <-
+  function(x, width = max(20, options()$width - 35), ...) {
+    cat("Time events from ")
+    printer(x$columns, x$terms, x$trained, width = width)
+    invisible(x)
+  }
+
+#' @rdname tidy.recipe
+#' @param x A `step_date_nearest` object.
+#' @export
+tidy.step_date_nearest <- function(x, ...) {
+  if (is_trained(x)) {
+    res <- tibble(terms = names(x$objects), rules = x$rules)
+  } else {
+    term_names <- sel2char(x$terms)
+    res <- tibble(terms = term_names, rules = x$rules)
+  }
+  res$id <- x$id
+  res
+}
---
+++ tests/testthat/test-date_nearest.R
@@ -0,0 +1,161 @@
+library(testthat)
+library(recipes)
+library(almanac)
+
+examples <- tibble(
+  date1 = as.Date("2021-08-16") + 1:20,
+  date2 = as.Date("2020-08-16") + 1:20,
+  numeric = 1:20
+)
+
+nearest_fun <- function(date, rule) {
+  pmin(
+    as.numeric(date - alma_previous(date, rule, inclusive = TRUE)),
+    as.numeric(alma_next(date, rule, inclusive = TRUE) - date)
+  )
+}
+
+test_that("time_event works", {
+  on_weekends <- weekly() %>% recur_on_weekends()
+  on_weekdays <- weekly() %>% recur_on_weekdays()
+
+  rules <- list(weekend = on_weekends, weekday = on_weekdays)
+  rec_spec <- recipe(~ date1, data = examples) %>%
+    step_date_nearest(date1, rules = rules) %>%
+    prep()
+
+  res <- bake(rec_spec, new_data = NULL)
+
+  expect_equal(names(res), c("date1_nearest_weekend", "date1_nearest_weekday"))
+
+  expect_equal(
+    nearest_fun(examples$date1, on_weekdays),
+    res$date1_nearest_weekday
+  )
+  expect_equal(
+    nearest_fun(examples$date1, on_weekends),
+    res$date1_nearest_weekend
+  )
+})
+
+test_that("time_event works with multiple columns", {
+  on_weekends <- weekly() %>% recur_on_weekends()
+  on_weekdays <- weekly() %>% recur_on_weekdays()
+
+  rules <- list(weekend = on_weekends, weekday = on_weekdays)
+  rec_spec <- recipe(~ date1 + date2, data = examples) %>%
+    step_date_nearest(date1, date2, rules = rules) %>%
+    prep()
+
+  res <- bake(rec_spec, new_data = NULL)
+
+  expect_equal(names(res), c("date1_nearest_weekend", "date1_nearest_weekday",
+                             "date2_nearest_weekend", "date2_nearest_weekday"))
+
+  expect_equal(
+    nearest_fun(examples$date1, on_weekdays),
+    res$date1_nearest_weekday
+  )
+  expect_equal(
+    nearest_fun(examples$date1, on_weekends),
+    res$date1_nearest_weekend
+  )
+  expect_equal(
+    nearest_fun(examples$date2, on_weekdays),
+    res$date2_nearest_weekday
+  )
+  expect_equal(
+    nearest_fun(examples$date2, on_weekends),
+    res$date2_nearest_weekend
+  )
+})
+
+
+test_that("time_event works with transform", {
+  on_weekends <- weekly() %>% recur_on_weekends()
+
+  rules <- list(weekend = on_weekends)
+
+  # Inverse
+  rec_spec <- recipe(~ date1, data = examples) %>%
+    step_date_nearest(date1, rules = rules, transform = "inverse") %>%
+    prep()
+
+  res <- bake(rec_spec, new_data = NULL)
+
+  expect_equal(
+    1 / (nearest_fun(examples$date1, on_weekends) + 0.5),
+    res$date1_nearest_weekend
+  )
+
+  # exp
+  rec_spec <- recipe(~ date1, data = examples) %>%
+    step_date_nearest(date1, rules = rules, transform = "exp") %>%
+    prep()
+
+  res <- bake(rec_spec, new_data = NULL)
+
+  expect_equal(
+    exp(nearest_fun(examples$date1, on_weekends)),
+    res$date1_nearest_weekend
+  )
+
+  # log
+  rec_spec <- recipe(~ date1, data = examples) %>%
+    step_date_nearest(date1, rules = rules, transform = "log") %>%
+    prep()
+
+  res <- bake(rec_spec, new_data = NULL)
+
+  expect_equal(
+    log(nearest_fun(examples$date1, on_weekends) + 0.5),
+    res$date1_nearest_weekend
+  )
+
+  # custom
+  custom_fun <- function(x) x / 2
+
+  rec_spec <- recipe(~ date1, data = examples) %>%
+    step_date_nearest(date1, rules = rules, transform = custom_fun) %>%
+    prep()
+
+  res <- bake(rec_spec, new_data = NULL)
+
+  expect_equal(
+    custom_fun(nearest_fun(examples$date1, on_weekends)),
+    res$date1_nearest_weekend
+  )
+})
+
+test_that("time_event errors", {
+  on_weekends <- weekly() %>% recur_on_weekends()
+  on_weekdays <- weekly() %>% recur_on_weekdays()
+
+  rules <- list(weekend = on_weekends, weekday = on_weekdays)
+
+  expect_error(
+    recipe(~ ., data = examples) %>%
+      step_date_nearest(numeric, rules = rules) %>%
+      prep()
+  )
+
+  expect_error(
+    recipe(~ ., data = examples) %>%
+      step_date_nearest(date1, rules = "wrong") %>%
+      prep()
+  )
+
+  expect_error(
+    recipe(~ ., data = examples) %>%
+      step_date_nearest(date1, rules = list(weekend = on_weekends, "Hello")) %>%
+      prep()
+  )
+
+  expect_error(
+    recipe(~ ., data = examples) %>%
+      step_date_nearest(date1, rules = list(weekend = on_weekends,
+                                          christmas = "2020-12-25")) %>%
+      prep()
+  )
+
+})
