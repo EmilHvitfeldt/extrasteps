4cb229ef5c841a010e060611d456e020239c14a6
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2022-01-04 11:58:50 -0800

    empty selectors step_difftime

--- R/0_imports.R
+++ R/0_imports.R
@@ -1,7 +1,7 @@
 #' @importFrom magrittr %>%
 #' @importFrom recipes add_step bake ellipse_check format_ch_vec prep
 #' @importFrom recipes format_selectors has_type is_trained prep printer rand_id
-#' @importFrom recipes recipes_eval_select sel2char step terms_select
+#' @importFrom recipes recipes_eval_select sel2char step
 #' @importFrom rlang na_chr enquos
 #' @importFrom tibble as_tibble tibble
 #' @importFrom generics tidy
