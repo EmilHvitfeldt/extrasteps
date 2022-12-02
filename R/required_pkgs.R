0b41b555543f9a44bc4482f0021a20431c92a628
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2022-12-01 16:06:00 -0800

    reexport required pkgs

---
+++ R/required_pkgs.R
@@ -0,0 +1,9 @@
+#' S3 methods for tracking which additional packages are needed for steps.
+#'
+#' Recipe-adjacent packages always list themselves as a required package so that
+#' the steps can function properly within parallel processing schemes.
+#' @param x A recipe step
+#' @return A character vector
+#' @name required_pkgs.extrasteps
+#' @keywords internal
+NULL
